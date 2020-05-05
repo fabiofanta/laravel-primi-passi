451abf7c43728890deb0950d8bfa04a7e6850ff7
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-05-05 18:54:47 +0200

    12-rel

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<footer>
+			<div class="container">
+				<div class="footer-left">
+					<div class="footer-logo">
+						<img src="{{asset('img/marchio-sito-test.png')}}" alt="La molisana">
+						<ul>
+							<li>Ragione sociale: La Molisana S.P.A</li>
+							<li>Sede legale: Contrada Colle delle Api, 100/A</li>
+							<li>86100 - Campobasso (CB)</li>
+							<li>Pec: lamolisana@pec.it</li>
+							<li>Tel: +39 0874 4981</li>
+							<li>Fax: +39 0874 629584</li>
+							<li>info@lamolisana.it</li>
+							<li>commerciale@lamolisana.it</li>
+							<li>export@lamolisana.it</li>
+							<li>numero verde 800818081</li>
+							<li>telefono 3801292455</li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer-center-top">
+					<h3>Pastificio</h3>
+					<ul>
+						<li>Il Pastificio</li>
+						<li>Grano decortinato a pietra</li>
+						<li>Il Molise c'è</li>
+						<li>Filiera Integrata</li>
+						<li>100 anni di pasta</li>
+						<li>Sartoria della pasta</li>
+						<li>Spaghetto Quadrato</li>
+						<li>Le Persona</li>
+					</ul>
+				</div>
+				<div class="footer-center-bottom">
+					<h3>Prodotti</h3>
+					<ul>
+						<li>Le Classiche</li>
+						<li>Le Integrali</li>
+						<li>Le Speciali</li>
+						<li>Le Biologiche</li>
+						<li>Le Gluten-Free</li>
+						<li>Le Semole</li>
+						<li>Le Extra di Lusso</li>
+					</ul>
+				</div>
+				<div class="footer-right">
+					<h3>Collezione da chef</h3>
+					<ul>
+						<li>Collezione da Chef</li>
+						<li>Grandi Cucine</li>
+						<li>Biologiche</li>
+						<li>Quadrate</li>
+					</ul>
+				</div>
+			</div>
+		</footer>
+
+	</body>
+</html>
