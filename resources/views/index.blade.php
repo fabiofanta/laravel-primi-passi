2302c149d4eb5cba2380987ad91a99bbca3e3452
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-05-05 16:23:48 +0200

    8-rel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
 		<title>Home</title>
 	</head>
 	<body>
